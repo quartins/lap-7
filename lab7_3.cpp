85ab9b50475b105a793075019c60ba9d98e437e9
nani <chotima.nani@gmail.com>
2023-12-11 20:39:01 +0700

    lap7-3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x)
+{
+	if( x <'A'|| x > 'Z')
+	{
+		return '0';
+	}
+	else{
+		if(x == 'A'){
+			return 'Z';
+		}
+		else {
+			return x-1;
+		}
+
+		}
 }
 
 int main(){
